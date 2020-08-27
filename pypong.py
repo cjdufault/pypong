5df3931dc4bdcc690ae5e3fbc10c2b6920d7a166
cjdufault <dufaultconnor@gmail.com>
2020-08-27 12:16:13 -0500

    Now detects when a player has scored and increments a counter to indicate their score

--- pypong.py
+++ pypong.py
@@ -1,6 +1,5 @@
 import pygame
 from os import path, environ
-#import os
 
 
 running = True
@@ -11,67 +10,59 @@ back_color = (0, 0, 0)
 last_collision_time = 0 # to lock out collision detection for 100ms after a collision
 collision_lockout = 100
 
+player1_score = 0
+player2_score = 0
+
 
 def main():
-    window = init_display("PyPong")
+    window = init_display("PyPong", path.join("assets", "pypong_icon.png"))
     play(window)
     
 
 def play(window):
-    global running
     global last_collision_time
     
     # create and draw ball and paddles
     ball = Ball(int(width / 100), (int(width / 2), int(height / 2)), int(height / 30))
     paddle1 = Paddle((int(width * 0.05), int(height / 2)), int(height / 5))
     paddle2 = Paddle((int(width * 0.95), int(height / 2)), int(height / 5))
+    object_list = [ball, paddle1, paddle2]
         
     while running:
         pygame.time.wait(10)
         
         # erase everything
-        window.fill(back_color, rect=ball.rect)
-        window.fill(back_color, rect=paddle1.rect)
-        window.fill(back_color, rect=paddle2.rect)
+        draw_objects(window, object_list, back_color)
         
         # update ball position and check for collisions
         ball.step_position()
         check_collide(ball, paddle1)
         check_collide(ball, paddle2)
-        if ball.position[1] - (ball.size / 2) <= 0 or ball.position[1] + (ball.size / 2) >= height:
+        if ball.position[1] - (ball.size / 2) <= 0 or ball.position[1] + (ball.size / 2) >= height: # check for collision w/ wall
             ball.wall_bounce()
         
+        # check if any player has scored
+        score_occurred = check_for_score(ball)
+        if score_occurred:
+            print(f"Player 1: {player1_score}\t Player 2: {player2_score}")
+            score_animation(window, ball, paddle1, paddle2)
+            
+        handle_keypress(paddle1, paddle2)
+        
         # check for quit events
-        quit_events = pygame.event.get(pygame.QUIT)
-        if len(quit_events) > 0:
-            running = False
-        
-        # handle key presses
-        keys = pygame.key.get_pressed()
-        
-        # paddle1 keys
-        if keys[pygame.K_w] and paddle1.position[1] - (paddle1.paddle_height / 2) >= 0:         # move up
-            paddle1.move(0 - (width / 150))
-        if keys[pygame.K_s] and paddle1.position[1] + (paddle1.paddle_height / 2) <= height:    # move down
-            paddle1.move(width / 150)
-
-        # paddle2 keys
-        if keys[pygame.K_UP] and paddle2.position[1] - (paddle2.paddle_height / 2) >= 0:        # move up
-            paddle2.move(0 - (width / 150))
-        if keys[pygame.K_DOWN] and paddle2.position[1] + (paddle2.paddle_height / 2) <= height: # move down
-            paddle2.move(width / 150)
+        listen_for_quit()
                              
         # draw everything and update display
-        window.fill(fore_color, rect=ball.rect)
-        window.fill(fore_color, rect=paddle1.rect)
-        window.fill(fore_color, rect=paddle2.rect)
+        draw_objects(window, object_list, fore_color)
         pygame.display.update()
+        pygame.event.clear()
             
 
 def check_collide(ball, paddle):
     global last_collision_time
     
-    if paddle.rect.colliderect(ball.rect) and pygame.time.get_ticks() - last_collision_time > collision_lockout:
+    # if ball collides w/ paddle & not within 100ms of a collision
+    if paddle.rect.colliderect(ball.rect) and pygame.time.get_ticks() - last_collision_time > collision_lockout: 
         # get position of ball and paddle in the y-axis, plus the height of the paddle
         ball_position_y = ball.position[1]
         paddle_position_y = paddle.position[1]
@@ -85,8 +76,67 @@ def check_collide(ball, paddle):
         last_collision_time = pygame.time.get_ticks() # last_collision_time is now
         
 
+def check_for_score(ball):
+    global player1_score
+    global player2_score
+    
+    score_occurred = False
+    
+    # left side -- player 2 scored
+    if ball.position[0] <= 0:
+        player2_score += 1
+        score_occurred = True
+        
+    # right side -- player 1 scored
+    elif ball.position[0] >= width:
+        player1_score += 1
+        score_occurred = True
+    
+    return score_occurred
+
+
+def score_animation(window, ball, paddle1, paddle2):
+    object_list = [ball, paddle1, paddle2]
+    
+    # show negative image
+    window.fill(fore_color)
+    draw_objects(window, object_list, back_color)
+    pygame.display.update()
+    pygame.time.wait(1000)
+
+    # return objects to starting locations
+    ball.reset_ball()
+    paddle1.reset_paddle()
+    paddle2.reset_paddle()
+    
+    window.fill(back_color)
+    draw_objects(window, object_list, fore_color)
+    pygame.display.update()
+
+        
+def draw_objects(window, object_list, color):
+    for obj in object_list:
+        window.fill(color, rect=obj.rect)
+    
+    
+def handle_keypress(paddle1, paddle2):
+    keys = pygame.key.get_pressed()
+    
+    # paddle1 keys
+    if keys[pygame.K_w] and paddle1.position[1] - (paddle1.paddle_height / 2) >= 0:         # move up
+        paddle1.move(0 - (width / 150))
+    if keys[pygame.K_s] and paddle1.position[1] + (paddle1.paddle_height / 2) <= height:    # move down
+        paddle1.move(width / 150)
+
+    # paddle2 keys
+    if keys[pygame.K_UP] and paddle2.position[1] - (paddle2.paddle_height / 2) >= 0:        # move up
+        paddle2.move(0 - (width / 150))
+    if keys[pygame.K_DOWN] and paddle2.position[1] + (paddle2.paddle_height / 2) <= height: # move down
+        paddle2.move(width / 150)
+        
+
 # returns a window for the game
-def init_display(title):
+def init_display(title, icon_path):
     global width
     global height
     
@@ -96,7 +146,7 @@ def init_display(title):
     pygame.display.set_caption(title)       # set title
     
     # load and set icon
-    icon = pygame.image.load(path.join("assets", "pypong_icon.png"))
+    icon = pygame.image.load(icon_path)
     pygame.display.set_icon(icon)
     
     # set window size w/ info from display_info
@@ -104,6 +154,15 @@ def init_display(title):
     height = int(display_info.current_h * 0.8)
     
     return pygame.display.set_mode((width, height))
+
+
+# returns True if QUIT event is registered 
+def listen_for_quit():
+    global running
+    
+    quit_events = pygame.event.get(pygame.QUIT)
+    if len(quit_events) > 0:
+        running = False
 
 
 class Ball:
@@ -141,6 +200,7 @@ class Ball:
     def reset_ball(self):
         self.x_spd = self.speed
         self.y_spd = 0
+        self.position = self.init_position
         self.rect = self.set_rect()
     
     # creates a Rect based on the size and position of the Ball
